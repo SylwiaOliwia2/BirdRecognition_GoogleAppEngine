87c5d3c716c5c30b85db56642e957a46975a880d
Sylwia <sylwia.kucharska11@gmail.com>
2019-09-08 17:30:13 +0200

    Restore initial setup.py file

--- app/server.py
+++ app/server.py
@@ -8,9 +8,13 @@ from io import BytesIO
 from fastai import *
 from fastai.vision import *
 
-export_file_url = 'https://drive.google.com/uc?export=download&id=11Vgj9UJJXTULLfKyPuYVejfvvBJ0ZhKZ'
+#model_file_url = 'https://drive.google.com/uc?export=download&id=11Vgj9UJJXTULLfKyPuYVejfvvBJ0ZhKZ'
+#model_file_name = 'model'
+#classes = ['Robin', 'Bullfinch', 'American Robin']
+model_file_url = 'https://www.dropbox.com/s/y4kl2gv1akv7y4i/stage-2.pth?raw=1'
 model_file_name = 'model'
-classes = ['Robin', 'Bullfinch', 'American Robin']
+classes = ['black', 'grizzly', 'teddys']
+
 path = Path(__file__).parent
 
 app = Starlette()

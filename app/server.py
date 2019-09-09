0d8165b3671f7e4a975b8f7042fdad54e6660f48
Sylwia <sylwia.kucharska11@gmail.com>
2019-09-09 15:28:47 +0200

    try with the initial link

--- app/server.py
+++ app/server.py
@@ -11,7 +11,7 @@ from fastai.vision import *
 #model_file_url = 'https://drive.google.com/uc?export=download&id=11Vgj9UJJXTULLfKyPuYVejfvvBJ0ZhKZ'
 #model_file_name = 'model'
 classes = ['Robin', 'Bullfinch', 'American Robin']
-model_file_url = 'https://www.dropbox.com/s/wx1mhaaag0rmh1f/stage-2.pth?raw=1'
+model_file_url = 'https://www.dropbox.com/s/y4kl2gv1akv7y4i/stage-2.pth?raw=1'
 model_file_name = 'model'
 #classes = ['black', 'grizzly', 'teddys']
 
